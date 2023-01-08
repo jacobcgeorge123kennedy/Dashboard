da7a753e846ff01327d9da09065c5afc4ffaa916
jacobcgeorge123kennedy <kennedyjacobjacob@gmail.com>
2023-01-08 23:29:00 +0530

    dashboard creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+  sidebar=true
+  isSidebarToggle(){
+    this.sidebar=!this.sidebar
+  }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  @Output() isSidebarToggle = new EventEmitter()
+  sidebartoggle(){
+    this.isSidebarToggle.emit()
+    setTimeout(()=>{
+      window.dispatchEvent(
+        new Event('resize')
+      )
+    },300);
+  }
+}
